4c43ff150c85be3ffbc04fe7c18376f99bf43b0a
kami1998 <919102450@qq.com>
2017-05-01 19:51:02 +0800

    Update 执行.cpp

--- 执行.cpp
+++ 执行.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main()
 {
 double r,s;
-cin>>"请输入圆的半径：">>r;//输入圆的半径
+  cout<<"请输入圆的半径："；
+cin>>r;//输入圆的半径
 s=count(r);//计算圆面积
 cout<<"圆的面积为："<<s<<endl;//输出圆面积
 system("pause");
