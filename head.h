a390a840f291fdceabc47507414adb2de9c8bb12
乐乐kami <919102450@qq.com>
2017-05-31 17:24:17 +0800

    text

--- head.h
+++ head.h
@@ -1,19 +1,24 @@
+#include <string.h>
 class Equation    //算式类
 {
 private:
 	char a[10];//用字符数组存放生成的算式
 public:
 	void generateExpresion();//用于生成运算式
-	friend int calculateResult(Equation &);//用于计算
-	int randomOperation();//生成随机运算符
-	int randomNumber();//生成随机数
-	char trans_operation();//转换运算符
+	friend double calculateResult(Equation &N);//用于计算
+	char randomNumber();//生成随机数
+	char trans_operation(int b);//转换运算符
 	void get();//输出算式
+	int lenth()
+	{
+		return strlen(a);
+	}
+	Equation()
+	{
+		for (int i=0; i < 10; i++)
+			a[i] = '\0'; 
+	}
 };
-int calculateResult(Equation &)
-{
-	
-}
 class Statistics
 {
 private:
@@ -21,8 +26,18 @@ private:
 public:
 	void judge(int ans, int s);//判断用户答案是否正确
 	int get();
+	Statistics()
+	{
+		right = 0;
+	}
 };
-void print(int c)//输出统计结果
+struct Num_Struct
 {
-
-}
+	double data[10];
+	 int top;
+};
+struct Operator_Struct
+{
+	char data[10];
+	int top;
+};
